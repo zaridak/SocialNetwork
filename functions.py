336255f8ef015b650f322dd42fafe0f0c4aad401
KostasAronis <aronis_k@yahoo.com>
2019-10-28 19:13:25 +0200

    asdf

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
 import os.path
 import shutil
-
+import networkx as nx
+import matplotlib.pyplot as plt
+from Centralities import CalculateCentrality
 def createFoldersIfNotExist():
     if not os.path.exists('GraphPics'):
         os.makedirs('GraphPics')
@@ -16,6 +18,92 @@ def deleteFilesIfExist():
     if os.path.exists("Centrality_Diagrams/"):
         shutil.rmtree('Centrality_Diagrams/')
         os.makedirs('Centrality_Diagrams')
+
+
+
+def write_Q3_file(allIntervals):
+    q3file = 0
+    #  Writing nodes of Q 3 to txt file
+    for tmp in allIntervals:
+        # tmp.intervalPrint()
+        print(tmp.getMinMax())
+        fileName = "Documents/Q3Nodes.txt"
+        q3file = open(fileName, 'a')
+        q3file.write(tmp.getMinMax())
+        print("I have " + str(tmp.getTotalNodes()) + " nodes:")
+        for aek in tmp.getIntervalNodes():
+            q3file.write(str(aek.getSourceID())+ " "+str(aek.getTargetID())+"\n")
+            # print(str(aek.getSourceID())+ " "+str(aek.getTargetID()))
+    q3file.close()
+
+def draw_graphs(allGraphsList):
+    j=1
+    for g in allGraphsList:
+        G=g['Ugraph']
+        nx.draw(G,with_labels = True)
+        plt.savefig("GraphPics/3pic_"+str(j)+".png")
+        j+=1
+        plt.close()
+
+def draw_centrality_graphs(allGraphsList):
+    for g in allGraphsList:
+        CalculateCentrality(g['interval'], g['Dgraph'],g['Ugraph'])
+
+def get_common_nodes(G,H):
+    R=G.copy()
+    R.remove_nodes_from(n for n in G if n not in H)
+    return R.nodes
+
+def get_graph_for_common_nodes(G, n):
+    R=G.copy()
+    R.remove_edges_from(edge for edge in G.edges if edge[0] not in n or edge[1] not in n) 
+    return R
+
+def get_CN(G):
+    CN=[]
+    for u in G.nodes:
+        for v in G.nodes:
+            if u != v:
+                commonNeighbors = list(nx.common_neighbors(G, u, v))
+                CN.append((u,v,len(commonNeighbors)))
+    return CN
+def get_GD(G):
+    GD=[]
+    for t in nx.all_pairs_shortest_path_length(G):
+        u=t[0]
+        for v in t[1]:
+            if v!=u:
+                GD.append((u,v,-t[1][v]))
+    return GD
+
+def get_JC(G):
+    JC=[]
+    for t in nx.jaccard_coefficient(G):
+        if t[0]!=t[1]:
+            JC.append(t)
+    return JC
+
+def get_A(G):
+    A=[]
+    for t in nx.adamic_adar_index(G):
+        if t[0]!=t[1]:
+            A.append(t)
+    return A
+
+def get_PA(G):
+    PA=[]
+    for t in nx.preferential_attachment(G):
+        if t[0] != t[1]:
+            PA.append(t)
+    return PA
+
+def get_success_percent(M0, G1):
+    correct = 0
+    total = len(M0)
+    for p in M0:
+        if(G1.has_edge(p[0],p[1])):
+            correct+=1
+    return correct/total
 
 
 # n_groups = 1
--- main.py
+++ main.py
@@ -40,12 +40,13 @@ allNodes, minTimeStamp, maxTimeStamp = inputFile.readFile("sx-stackoverflow-10k.
 print("Totally " + str(len(allNodes)) + " nodes")
 dif = (maxTimeStamp - minTimeStamp)
 step = dif / N
-
+# Question 1 Finding tMin and tMax
 print("Min: " + str(minTimeStamp) + " || Max: " + str(maxTimeStamp))  # + "\nsec dif = " + str(dif))
 print("Splitting to " + str(N) + " intervals step: " + str(step))
 
 allIntervals = []
 
+# Question 2 seperating into intervals
 tmpMin = minTimeStamp
 for i in range(N):
     allIntervals.append(Interval(tmpMin, tmpMin + step))
@@ -66,33 +67,17 @@ for tmpNode in allNodes:
             tmpNode.setInterval(tmpInter)
 print("Printing NODES @ INTERNALS ")
 
-def write_Q3_file():
-    q3file = 0
-    #  Writing nodes of Q 3 to txt file
-    for tmp in allIntervals:
-        # tmp.intervalPrint()
-        print(tmp.getMinMax())
-        fileName = "Documents/Q3Nodes.txt"
-        q3file = open(fileName, 'a')
-        q3file.write(tmp.getMinMax())
-        print("I have " + str(tmp.getTotalNodes()) + " nodes:")
-        for aek in tmp.getIntervalNodes():
-            q3file.write(str(aek.getSourceID())+ " "+str(aek.getTargetID())+"\n")
-            # print(str(aek.getSourceID())+ " "+str(aek.getTargetID()))
-    q3file.close()
-#write_Q3_file()
 
+write_Q3_file(allIntervals)
 
 # iterate all intervals and create Graphs for Q 3
 allGraphsList=[]
 
 #create graphs for each interval
-j=0
 for tmpInt in allIntervals: # for each interval
     Gd = nx.DiGraph()
     Gu = nx.Graph()
     if tmpInt.hasNodes() is True:  # null check
-        j+=1
         for tmpNode in tmpInt.getIntervalNodes():
             Gd.add_edge(tmpNode.getSourceID(),tmpNode.getTargetID())
             Gu.add_edge(tmpNode.getSourceID(),tmpNode.getTargetID())
@@ -102,74 +87,8 @@ for tmpInt in allIntervals: # for each interval
             "Ugraph":Gu
         })
 
-def draw_graphs():
-    for g in allGraphsList:
-        G=g['Ugraph']
-        nx.draw(G,with_labels = True)
-        plt.savefig("GraphPics/3pic_"+str(j)+".png")
-        plt.close()
-#draw_graphs()
-
-def draw_centrality_graphs():
-    for g in allGraphsList:
-        CalculateCentrality(g['interval'], g['Dgraph'],g['Ugraph'])
-#draw_centrality_graphs()
-
-def get_common_nodes(G,H):
-    R=G.copy()
-    R.remove_nodes_from(n for n in G if n not in H)
-    return R.nodes
-
-def get_graph_for_common_nodes(G, n):
-    R=G.copy()
-    R.remove_edges_from(edge for edge in G.edges if edge[0] not in n or edge[1] not in n) 
-    return R
-
-def get_CN(G):
-    CN=[]
-    for u in G.nodes:
-        for v in G.nodes:
-            if u != v:
-                commonNeighbors = list(nx.common_neighbors(G, u, v))
-                CN.append((u,v,len(commonNeighbors)))
-    return CN
-def get_GD(G):
-    GD=[]
-    for t in nx.all_pairs_shortest_path_length(G):
-        u=t[0]
-        for v in t[1]:
-            if v!=u:
-                GD.append((u,v,-t[1][v]))
-    return GD
-
-def get_JC(G):
-    JC=[]
-    for t in nx.jaccard_coefficient(G):
-        if t[0]!=t[1]:
-            JC.append(t)
-    return JC
-
-def get_A(G):
-    A=[]
-    for t in nx.adamic_adar_index(G):
-        if t[0]!=t[1]:
-            A.append(t)
-    return A
-
-def get_PA(G):
-    PA=[]
-    for t in nx.preferential_attachment(G):
-        if t[0] != t[1]:
-            PA.append(t)
-    return PA
-
-def get_success_percent(M0, G1):
-    correct = 0
-    total = len(M0)
-    for p in M0:
-        if(G1.has_edge(p[0],p[1])):
-            correct+=1
-    return correct/total
+draw_graphs(allGraphsList)
+draw_centrality_graphs(allGraphsList)
 
 
 for idx in range(len(allGraphsList)-1):
